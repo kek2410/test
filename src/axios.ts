7d0030971802a4f0d8a53d574278df73815ed0c5
kek2410 <kek2410@naver.com>
2019-11-17 22:46:19 +0900

    aixos commit

--- src/axios.ts
+++ src/axios.ts
@@ -2,17 +2,19 @@ import axios from "axios";
 
 // axios.defaults.baseURL = "https://reqres.in";
 
-const customAxios = async (url: string, params?: any): Promise<any> => {
+const customAxios = async (url: string, params?: any, method?: string): Promise<any> => {
   let result: any = null;
 
   console.log("url: ", url);
 
   // axios.defaults.headers.post[""]
-
   // params.push(headers: {"Access-Control-Allow-Origin": "*"});
 
+  
   if (params === undefined || params === {}) params = null;
   if (Object.keys(params).length === 0) params = null;
+  
+  if (method === undefined || method === null || method === "") method = "get";
 
   console.log(params);
 
@@ -23,18 +25,13 @@ const customAxios = async (url: string, params?: any): Promise<any> => {
   // });
   try {
     let config: any = null;
-    if (params === null) {
-      config = {
-        method: "post",
-        url: url
-      };
-    } else {
-      config = {
-        method: "post",
-        url: url,
-        data: params
-      };
-    }
+
+    config = {
+      method: method,
+      url: url,
+      data: params
+    };
+    
     // console.log(response);
     await axios(config)
       .then(function(response): void {
